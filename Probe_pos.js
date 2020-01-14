2a13ee161880cf3688b4fb092e082e09683802db
Langlois William <Langlois.William.pro@gmail.com>
2020-01-14 11:47:55 +0100

    Couleur des marker en fonction du caractère actif d'une sonde

--- Probe_pos.js
+++ Probe_pos.js
@@ -27,9 +27,35 @@ function GetProbe_pos()
 
     }
 
-function DispProbe_pos(name,pos_x,pos_y)
-  {
-    L.marker([pos_x, pos_y],{})
-      .bindPopup('<b>' +"Probe name :"+name+'</b>')
-      .addTo(mymap);
+function DispProbe_pos(name,pos_x,pos_y,isactive)
+  { 
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+      var redIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+      if(isactive==1)
+      {
+        L.marker([pos_x, pos_y],{icon: greenIcon})
+        .bindPopup('<b>' +"Probe name :"+name+'</b>')
+        .addTo(mymap);
+      }
+      else
+      {
+        L.marker([pos_x, pos_y],{icon: redIcon})
+        .bindPopup('<b>' +"Probe name :"+name+'</b>')
+        .addTo(mymap);
+      }
+   
   }
