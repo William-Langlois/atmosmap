0fee586d15dac2f1ec51e7859bf8f1bed2244802
Langlois William <Langlois.William.pro@gmail.com>
2020-01-14 16:30:46 +0100

    attempt to print measures

--- Probe_pos.js
+++ Probe_pos.js
@@ -4,10 +4,12 @@ $(document).ready(function() {
     GetProbe_pos();
 });
 
+function testdelay(){
+    console.log("Une action")
+}
 
 function GetProbe_pos()
     {
-
         $.ajax({
             type:"GET",
             url:"http://10.176.129.94:5000/atmos/probe/",
@@ -16,18 +18,42 @@ function GetProbe_pos()
                 console.log(data);
                 for(i=0;i<data.length;i++)
                 {
-                    DispProbe_pos(data[i]['name'],data[i]['pos_x'],data[i]['pos_y'],data[i]['active'])
+                    var id_probes= new Array
+                    id_probes.push(i)
+                    DispProbe_pos(i,data[i]['name'],data[i]['pos_x'],data[i]['pos_y'],data[i]['active'])
                 }
+               //DispProbe_measure(id_probes)
             },
             error: function(){
-                console.log("failed to acquire data");
+                console.log("failed to acquire probes localisations");
             },
             dataType:'json',            
         });
 
     }
 
-function DispProbe_pos(name,pos_x,pos_y,isactive)
+    /*function DispProbe_measure(id_probes)
+    {
+
+        $.ajax({
+            type:"GET",
+            for(j=0;j<id_probes.length;j++){
+            url:"http://10.176.129.94:5000/atmos/measure/last/"+id_probes[j]
+            }
+            headers:{"Access-Control-Allow-Header": "*"},
+            success: function(data_measure){
+                console.log(data_measure)
+            },
+            error: function(){
+                console.log("failed to acquire probes measures")
+            },
+            dataType:'json',            
+        });
+
+    }*/
+
+
+function DispProbe_pos(id_probe,name,pos_x,pos_y,isactive)
   { 
     var greenIcon = new L.Icon({
         iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
@@ -47,13 +73,13 @@ function DispProbe_pos(name,pos_x,pos_y,isactive)
       });
       if(isactive==1)
       {
-        L.marker([pos_x, pos_y],{icon: greenIcon})
+       L.marker([pos_x, pos_y],{icon: greenIcon})
         .bindPopup('<b>' +"Probe name :"+name+'</b>')
         .addTo(mymap);
       }
       else
       {
-        L.marker([pos_x, pos_y],{icon: redIcon})
+       L.marker([pos_x, pos_y],{icon: redIcon})
         .bindPopup('<b>' +"Probe name :"+name+'</b>')
         .addTo(mymap);
       }
